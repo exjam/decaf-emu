7c5c33963e0a79ed81f1b4f49b472d217a382167
James Benton <james.benton2@gmail.com>
2017-11-02 10:49:48 +0000

    cafe: Improve cafe::StackObject.
    Use the correct alignment for Type, and call constructor / destructor.

--- src/libdecaf/src/cafe/cafe_stackobject.h
+++ src/libdecaf/src/cafe/cafe_stackobject.h
@@ -10,7 +10,7 @@ namespace cafe
 template<typename Type>
 class StackObject : public virt_ptr<Type>
 {
-   static constexpr auto AlignedSize = align_up(static_cast<uint32_t>(sizeof(Type)), 4);
+   static constexpr auto AlignedSize = align_up(static_cast<uint32_t>(sizeof(Type)), std::max(alignof(Type), 4));
 
 public:
    StackObject()
@@ -26,6 +26,7 @@ public:
 
       core->gpr[1] = newStackTop.getAddress();
       mAddress = virt_addr { newStackTop + 8 };
+      new (getRawPointer()) Type { };
    }
 
    ~StackObject()
@@ -35,6 +36,8 @@ public:
       auto newStackTop = virt_addr { core->gpr[1] + AlignedSize };
       auto ptrAddr = oldStackTop + 8;
       decaf_check(mAddress == ptrAddr);
+
+      getRawPointer()->~Type();
 
       core->gpr[1] = newStackTop.getAddress();
       memmove(virt_cast<void *>(newStackTop).getRawPointer(),
