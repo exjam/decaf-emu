357e4d1d42e3d95ce593becd6cef7e97a48ea81f
James Benton <james.benton2@gmail.com>
2018-01-11 09:13:54 +0000

    cafe/StackObject: Use std::uninitialized_default_construct_n/destroy_n.

--- src/libdecaf/src/cafe/cafe_stackobject.h
+++ src/libdecaf/src/cafe/cafe_stackobject.h
@@ -4,14 +4,17 @@
 #include <common/decaf_assert.h>
 #include <libcpu/cpu.h>
 #include <libcpu/be2_struct.h>
+#include <memory>
 
 namespace cafe
 {
 
-template<typename Type>
+template<typename Type, size_t NumElements = 1>
 class StackObject : public virt_ptr<Type>
 {
-   static constexpr auto AlignedSize = align_up(static_cast<uint32_t>(sizeof(Type)), std::max(alignof(Type), 4u));
+   static constexpr auto
+   AlignedSize = align_up(static_cast<uint32_t>(sizeof(Type) * NumElements),
+                          std::max<std::size_t>(alignof(Type), 4u));
 
 public:
    StackObject()
@@ -27,18 +30,18 @@ public:
 
       core->gpr[1] = newStackTop.getAddress();
       mAddress = virt_addr { newStackTop + 8 };
-      new (getRawPointer()) Type { };
+      std::uninitialized_default_construct_n(getRawPointer(), NumElements);
    }
 
    ~StackObject()
    {
+      std::destroy_n(getRawPointer(), NumElements);
+
       auto core = cpu::this_core::state();
       auto oldStackTop = virt_addr { core->gpr[1] };
       auto newStackTop = virt_addr { core->gpr[1] + AlignedSize };
       auto ptrAddr = oldStackTop + 8;
       decaf_check(mAddress == ptrAddr);
-
-      getRawPointer()->~Type();
 
       core->gpr[1] = newStackTop.getAddress();
       memmove(virt_cast<void *>(newStackTop).getRawPointer(),
