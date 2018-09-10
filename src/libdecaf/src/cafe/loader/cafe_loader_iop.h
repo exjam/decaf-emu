6fb64ff62096170f4817320839b0f7db476ce7c9
James Benton <james.benton2@gmail.com>
2018-09-10 08:40:59 +0100

    aaa

--- src/libdecaf/src/cafe/loader/cafe_loader_iop.h
+++ src/libdecaf/src/cafe/loader/cafe_loader_iop.h
@@ -1,9 +1,31 @@
 #pragma once
+#include "cafe/kernel/cafe_kernel_processid.h"
+#include "ios/mcp/ios_mcp_enum.h"
 
 namespace cafe::loader::internal
 {
 
 void
+LiInitIopInterface();
+
+ios::Error
+LiLoadAsync(std::string_view name,
+            virt_ptr<void> outBuffer,
+            uint32_t outBufferSize,
+            uint32_t pos,
+            ios::mcp::MCPFileType fileType,
+            cafe::kernel::RamPartitionId rampid);
+
+ios::Error
+LiWaitIopComplete(uint32_t *outBytesRead);
+
+ios::Error
+LiWaitIopCompleteWithInterrupts(uint32_t *outBytesRead);
+
+void
 LiCheckAndHandleInterrupts();
+
+void
+initialiseStaticIopData();
 
 } // namespace cafe::loader::internal
